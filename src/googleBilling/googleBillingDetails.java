442b26c027ec2db4e49ccaa993207e7a53be92eb
briley-user <briley.personal@gmail.com>
2018-07-28 10:32:29 -0400

    Update ver 1.1

--- src/googleBilling/googleBillingDetails.java
+++ src/googleBilling/googleBillingDetails.java
@@ -9,8 +9,6 @@ import com.google.api.services.cloudbilling.Cloudbilling;
 import com.google.api.services.cloudbilling.model.ProjectBillingInfo;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.net.*;
 import java.util.*;
 
 
@@ -20,21 +18,23 @@ public class googleBillingDetails {
 	        GoogleCredential credential = GoogleCredential.getApplicationDefault();
 
 	        if (credential.createScopedRequired() ) {
-	                 credential = credential.createScoped(Arrays.asList("https://developers.google.com/resources/api-libraries/download/cloudbilling/v1/java"));
+	                 credential = credential.createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
 	        }
-
+	        //"https://www.googleapis.com/auth/cloud-platform"
+	        //https://developers.google.com/resources/api-libraries/download/cloudbilling/v1/java
 	        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 	        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 	        Cloudbilling cloudbillingService = new Cloudbilling.Builder(httpTransport, jsonFactory, credential)
-	                .setApplicationName("Foodie Directions 07-22-2018")
+	                .setApplicationName("Google Cloud Platform Sample")
 	                .build();
 
 
-	        String name= "projects/Foodie Directions 07-22-2018"; // change this to my project name in the google cloud
+	        String name= "projects/foodie-directions-07-22-2018"; // change this to my project name in the google cloud
 	         Cloudbilling.Projects.GetBillingInfo request = cloudbillingService.projects().getBillingInfo(name);
+	         System.out.println("request:"+request.toString());
 	         ProjectBillingInfo response = request.execute();
 
-	         System.out.println(response.getName());
+	         System.out.println(response.getBillingAccountName());
 	        }
 
 
